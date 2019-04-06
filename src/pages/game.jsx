1352d7d4e265b677f07f0d0ad22ed77a30cdc597
Kamil Malinowski <malinowskikamil91@gmail.com>
2019-04-06 08:38:58 +0200

    added timer

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { api_url } from "../utils/defaults";
 import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Layout from "../components/layout";
 import { Button, ButtonToolbar, ListGroup, ProgressBar } from "react-bootstrap";
 
@@ -10,34 +10,53 @@ class Game extends Component {
     questions: [],
     questionsAmount: 10,
     activeQuestion: 0,
-    score: 0
+    score: 0,
+    timer: 15
   };
 
   componentDidMount() {
     this.handleGetQuestions();
   }
+  
+  componentWillUnmount() {
+    clearInterval(this.tick);
+  }
+  
 
   handleGetQuestions = () => {
     const { level, category } = this.props.match.params;
     axios
       .get(
         `${api_url}/api.php?amount=${
-          this.state.questionsAmount
+        this.state.questionsAmount
         }&difficulty=${level}&category=${category}`
       )
-      .then(({ data: { results } }) => this.setState({ questions: results }));
+      .then(({ data: { results } }) => {
+        const shuffle = a => {
+          for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+          }
+          return a;
+        };
+        results.map(item => 
+          item.answers = shuffle(item.incorrect_answers.concat(item.correct_answer))
+        )
+        this.setState({ questions: results })
+        this.tick()
+      });
   };
 
   increaseScore = () => {
-    let { score } = this.state,
+    let { score, timer } = this.state,
       { level } = this.props.match.params;
     switch (level) {
       case "easy":
-        return this.setState({ score: score + 5 });
+        return this.setState({ score: score + (5 * timer) });
       case "medium":
-        return this.setState({ score: score + 10 });
+        return this.setState({ score: score + (10 * timer) });
       case "hard":
-        return this.setState({ score: score + 15 });
+        return this.setState({ score: score + (15 * timer) });
       default:
         break;
     }
@@ -67,15 +86,8 @@ class Game extends Component {
   };
 
   renderAnswers = (data, activeQuestion) => {
-    const shuffle = a => {
-      for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-      }
-      return a;
-    };
+
     if (activeQuestion < this.state.questionsAmount) {
-      let answers = shuffle(data.incorrect_answers.concat(data.correct_answer));
       return (
         <div className="questions-container">
           <h1
@@ -83,7 +95,7 @@ class Game extends Component {
             dangerouslySetInnerHTML={{ __html: data.question }}
           />
           <ListGroup as="ul" className="questions-list">
-            {answers.map(item => (
+            {data.answers.map(item => (
               <ListGroup.Item
                 as="li"
                 key={item}
@@ -135,7 +147,7 @@ class Game extends Component {
       target.classList.remove("success");
       target.classList.remove("danger");
       answer === correct_answer && this.increaseScore();
-      this.setState({ activeQuestion: activeQuestion + 1 });
+      this.setState({ activeQuestion: activeQuestion + 1, timer: 15 });
     }, 300);
   };
 
@@ -143,6 +155,7 @@ class Game extends Component {
     this.setState({ activeQuestion: 0, score: 0 });
     this.handleGetQuestions();
   };
+
   renderTitle = () => {
     const { questions, questionsAmount, activeQuestion } = this.state;
     if (questions.length === 0) {
@@ -154,8 +167,22 @@ class Game extends Component {
     }
   };
 
+  tick = () => {
+    setInterval(() => {
+      const { timer, activeQuestion } = this.state;
+      if (timer === 0) {
+        clearInterval(this.tick);
+        this.setState({ activeQuestion: activeQuestion + 1, timer: 15 })
+      } else {
+        this.setState({ timer: timer - 1 })
+      }
+    }, 1000)
+  };
+
+
+
   render() {
-    const { questions, activeQuestion, questionsAmount, score } = this.state;
+    const { questions, activeQuestion, questionsAmount, score, timer } = this.state;
     return (
       <Layout title={this.renderTitle()}>
         <div className="game-container">
@@ -166,6 +193,7 @@ class Game extends Component {
           {activeQuestion < questionsAmount && (
             <p className="active-score"> Active score: {score}</p>
           )}
+          <p>Time's left: {timer}</p>
           {questions.length > 0 &&
             this.renderAnswers(questions[activeQuestion], activeQuestion)}
         </div>
